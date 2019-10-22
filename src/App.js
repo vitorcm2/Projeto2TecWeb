a6afd0efe0642db79064dc8d2bdec33893b8801c
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2019-10-22 14:26:06 -0300

    sortby e comeco do search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import InputGroup from 'react-bootstrap/InputGroup'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import lol from "riot-lol";
 
@@ -11,6 +18,9 @@ function App() {
   const [champions, setChampions] = useState();
   const [items, setItem] = useState();
   const [runes, setRune] = useState();
+  const [showChampions, setShowChampions] = useState(false);
+  const [showItens, setShowItens] = useState(false);
+  const [showRunes, setShowRunes] = useState(false);
   const item_url = "http://ddragon.leagueoflegends.com/cdn/9.20.1/img/item/"
 
   const REGION = leagueAPI.CONSTANTS.REGION;
@@ -21,68 +31,107 @@ function App() {
 
   useEffect(() => {
     lol
-    .getChampions()
-    .then(champion => {
-      console.log(champion);
-      setChampions(champion);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .getChampions()
+      .then(champion => {
+        console.log(champion);
+        setChampions(champion);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, [])
 
   useEffect(() => {
     lol
-    .getItems()
-    .then(item => {
-      console.log(item);
-      setItem(item);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .getItems()
+      .then(item => {
+        console.log(item);
+        setItem(item);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, [])
 
   useEffect(() => {
     lol
-    .getRunes()
-    .then(rune => {
-      console.log(rune);
-      setRune(rune);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .getRunes()
+      .then(rune => {
+        console.log(rune);
+        setRune(rune);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, [])
 
-  
-  if (champions && items) {
+  // useEffect(() => {
+  //   lol
+  //     .getChampion("Aatrox")
+  //     .then(champion => {
+  //       console.log(champion);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }, [])
+
+  if (champions && items && runes) {
     return (
       <div className="App">
+        <Jumbotron>
+          <h1>Welcome to LeagueStats!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+  </p>
+          <p>
+            <a href="https://developer.riotgames.com/"><Button variant="primary">Riot API</Button></a>
+          </p>
+        </Jumbotron>
+        <Form>
+          {['checkbox'].map(type => (
+            <div key={`inline-${type}`} className="mb-3">
+              <Form.Check inline label="Champions" type={type} id={`inline-${type}-1`} onClick={() => setShowChampions(!showChampions)} />
+              <Form.Check inline label="Itens" type={type} id={`inline-${type}-2`} onClick={() => setShowItens(!showItens)} />
+              <Form.Check inline label="Runas" type={type} id={`inline-${type}-3`} onClick={() => setShowRunes(!showRunes)} />
+            </div>
+          ))}
+        </Form>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Search Champion / Item / Rune"
+            aria-label="Search Champion / Item / Rune"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Append>
+            <Button variant="outline-secondary">Search</Button>
+          </InputGroup.Append>
+        </InputGroup>
         <header className="App-header">
-          {champions.map((obj, index) => {
+          {showChampions && champions.map((obj, index) => {
             return (
               <div key={obj.key}>
-              <div>{obj.name}</div>
-              <img src={obj.imageUrl} alt={`img ${obj.name}`} height="60" width="60"/>
+                <div>{obj.name}</div>
+                <img src={obj.imageUrl} alt={`img ${obj.name}`} height="60" width="60" />
               </div>
             )
           })
           }
-          {items.map((obj, index) => {
+          {showItens && items.map((obj, index) => {
             return (
               <div key={obj.id}>
-              <div>{obj.name}</div>
-              <img src={item_url + obj.image.full} alt={`img ${obj.name}`} height="60" width="60"/>
+                <div>{obj.name}</div>
+                <img src={item_url + obj.image.full} alt={`img ${obj.name}`} height="60" width="60" />
               </div>
             )
           })
           }
-          {runes.map((obj, index) => {
+          {showRunes && runes.map((obj, index) => {
             return (
               <div key={obj.id}>
-              <div>{obj.name}</div>
-              <div>{obj.description}</div>
+                <div>{obj.name}</div>
+                <div>{obj.description}</div>
               </div>
             )
           })
@@ -102,7 +151,7 @@ function App() {
             Learn React
           </a>
         </header>
-      </div>
+      </div >
     );
   }
 
