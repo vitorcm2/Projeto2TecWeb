3c4d6b86131935e9caea6e1ca4b5d1036b3a9319
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2019-10-22 16:56:57 -0300

    search e overlaytrigger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import InputGroup from 'react-bootstrap/InputGroup'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import lol from "riot-lol";
@@ -15,13 +13,15 @@ function App() {
   const leagueAPI = require('node-league-api');
 
   const RIOT_API_KEY = 'RGAPI-1c7b8473-c7ee-4010-acdd-df71ff57e518';
-  const [champions, setChampions] = useState();
+  const [champions, setChampion] = useState();
   const [items, setItem] = useState();
-  const [runes, setRune] = useState();
+  const [summoners, setSummoner] = useState();
   const [showChampions, setShowChampions] = useState(false);
   const [showItens, setShowItens] = useState(false);
-  const [showRunes, setShowRunes] = useState(false);
+  const [showSummoners, setShowSummoners] = useState(false);
+  const [search, setSearch] = useState("");
   const item_url = "http://ddragon.leagueoflegends.com/cdn/9.20.1/img/item/"
+  const summoner_url = "http://ddragon.leagueoflegends.com/cdn/9.20.1/img/spell/"
 
   const REGION = leagueAPI.CONSTANTS.REGION;
 
@@ -34,7 +34,7 @@ function App() {
       .getChampions()
       .then(champion => {
         console.log(champion);
-        setChampions(champion);
+        setChampion(champion);
       })
       .catch(err => {
         console.log(err);
@@ -55,10 +55,18 @@ function App() {
 
   useEffect(() => {
     lol
-      .getRunes()
-      .then(rune => {
-        console.log(rune);
-        setRune(rune);
+      .getSummoners()
+      .then(summoner => {
+        console.log(summoner)
+        const clean = []
+        const not_exist_img = [2, 3, 11, 15, 16]
+        for (var i = 0; i < summoner.length; i++) {
+          if (!not_exist_img.includes(i)) {
+            clean.push(summoner[i])
+          }
+        }
+
+        setSummoner(clean);
       })
       .catch(err => {
         console.log(err);
@@ -76,7 +84,19 @@ function App() {
   //     });
   // }, [])
 
-  if (champions && items && runes) {
+  if (champions && items && summoners) {
+
+    const filteredChampions = champions.filter(item => {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    const filteredItens = items.filter(item => {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    const filteredSummoners = summoners.filter(item => {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    });
     return (
       <div className="App">
         <Jumbotron>
@@ -84,7 +104,7 @@ function App() {
           <p>
             This is a simple hero unit, a simple jumbotron-style component for calling
             extra attention to featured content or information.
-  </p>
+          </p>
           <p>
             <a href="https://developer.riotgames.com/"><Button variant="primary">Riot API</Button></a>
           </p>
@@ -94,50 +114,75 @@ function App() {
             <div key={`inline-${type}`} className="mb-3">
               <Form.Check inline label="Champions" type={type} id={`inline-${type}-1`} onClick={() => setShowChampions(!showChampions)} />
               <Form.Check inline label="Itens" type={type} id={`inline-${type}-2`} onClick={() => setShowItens(!showItens)} />
-              <Form.Check inline label="Runas" type={type} id={`inline-${type}-3`} onClick={() => setShowRunes(!showRunes)} />
+              <Form.Check inline label="Summoners" type={type} id={`inline-${type}-3`} onClick={() => setShowSummoners(!showSummoners)} />
             </div>
           ))}
         </Form>
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="Search Champion / Item / Rune"
-            aria-label="Search Champion / Item / Rune"
-            aria-describedby="basic-addon2"
-          />
-          <InputGroup.Append>
-            <Button variant="outline-secondary">Search</Button>
-          </InputGroup.Append>
-        </InputGroup>
+        <input
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+          value={search}
+          placeholder="Search..."
+        />
+
+
         <header className="App-header">
-          {showChampions && champions.map((obj, index) => {
+          {showChampions && filteredChampions.map((obj, index) => {
             return (
-              <div key={obj.key}>
-                <div>{obj.name}</div>
-                <img src={obj.imageUrl} alt={`img ${obj.name}`} height="60" width="60" />
-              </div>
+              <OverlayTrigger
+                key={obj.key}
+                placement={"top"}
+                overlay={
+                  <Tooltip id={`tooltip-${"top"}`}>
+                    {obj.description}
+                  </Tooltip>
+                }>
+                <div>
+                  <div>{obj.name}</div>
+                  <img src={obj.imageUrl} alt={`img ${obj.name}`} height="60" width="60" />
+                </div>
+              </OverlayTrigger>
             )
           })
           }
-          {showItens && items.map((obj, index) => {
+          {showItens && filteredItens.map((obj, index) => {
             return (
-              <div key={obj.id}>
-                <div>{obj.name}</div>
-                <img src={item_url + obj.image.full} alt={`img ${obj.name}`} height="60" width="60" />
-              </div>
+              <OverlayTrigger
+                key={obj.id}
+                placement={"top"}
+                overlay={
+                  <Tooltip id={`tooltip-${"top"}`}>
+                    {obj.plaintext}
+                  </Tooltip>
+                }>
+                <div>
+                  <div>{obj.name}</div>
+                  <img src={item_url + obj.image.full} alt={`img ${obj.name}`} height="60" width="60" />
+                </div>
+              </OverlayTrigger>
             )
           })
           }
-          {showRunes && runes.map((obj, index) => {
+          {showSummoners && filteredSummoners.map((obj, index) => {
             return (
-              <div key={obj.id}>
-                <div>{obj.name}</div>
-                <div>{obj.description}</div>
-              </div>
+              <OverlayTrigger
+                key={obj.id}
+                placement={"top"}
+                overlay={
+                  <Tooltip id={`tooltip-${"top"}`}>
+                    {obj.description}
+                  </Tooltip>
+                }>
+                <div>
+                  <div>{obj.name}</div>
+                  <img src={summoner_url + obj.image.full} alt={`img ${obj.name}`} height="60" width="60" />
+                </div>
+              </OverlayTrigger>
             )
           })
           }
 
-          <img src={champions[1].imageUrl} className="App-logo" alt="logo" />
+          {/* <img src={champions[1].imageUrl} className="App-logo" alt="logo" />
           <code>{champions[1].name}</code>
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -149,7 +194,9 @@ function App() {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+
+
         </header>
       </div >
     );
